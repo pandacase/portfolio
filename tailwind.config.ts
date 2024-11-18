332742b19b4a6935730615be874996c067ea35f7
pandacase <2227119810@qq.com>
2024-11-18 17:03:18 +0800

    initialize the repo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,11 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: { 
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
       },
     },
   },
