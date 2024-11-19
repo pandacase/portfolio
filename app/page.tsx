ef3aa2ed9b01a60d4e3d49488ce8376ad261fe17
pandacase <2227119810@qq.com>
2024-11-19 15:37:18 +0800

    feature: up to 32:52

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl px-5">
-        <h1>hello</h1>
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
 }
+ 
